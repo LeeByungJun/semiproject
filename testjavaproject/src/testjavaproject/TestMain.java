0793052ded9dde837248dd050ac69c2e50d500d7
LeeByungJun <semicolon92@naver.com>
2018-03-07 10:47:53 +0900

    첫번째 커밋

---
+++ testjavaproject/src/testjavaproject/TestMain.java
@@ -0,0 +1,11 @@
+package testjavaproject;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("수정전");
+		System.out.print("아아아아아아아");
+	}
+
+}
